ed38c01467ffab3b255dc2055a44719fa33cb68d
Thomasdalsae <72367829+Thomasdalsae@users.noreply.github.com>
2023-11-01 17:59:55 +0100

    Keep Testing

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -10,31 +10,34 @@ public class MeshGenerator : MonoBehaviour
     private Mesh mesh;
     private Vector3[] vertices;
     private int[] triangles;
+    
+    public int xCellCount = 5;  // Number of quads in the x direction
+    public int zCellCount = 5;  // Number of quads in the z direction
 
     public TextAsset textAsset;
     public int gridSize = 5;  // Resolution in meters
-public int xCellCount = 5;  // Number of quads in the x direction
-public int zCellCount = 5;  // Number of quads in the z direction
-    void Start()
-    {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-        GetCoordFromFile(textAsset);
-        CreateShape();
-        UpdateMesh();
-    }
 
-    void GetCoordFromFile(TextAsset path)
+   
+void Start()
+{
+    mesh = new Mesh();
+    GetComponent<MeshFilter>().mesh = mesh;
+    GetCoordFromFile(textAsset);
+    UpdateMesh();
+}
+
+
+    void GetCoordFromFile(TextAsset textAsset)
     {
         try
         {
             string[] lines = textAsset.text.Split('\n');
-
+    
             float minX = float.MaxValue;
             float minY = float.MaxValue;
             float maxX = float.MinValue;
             float maxY = float.MinValue;
-
+    
             foreach (var line in lines)
             {
                 string[] parts = line.Split(' ');
@@ -49,31 +52,30 @@ public int zCellCount = 5;  // Number of quads in the z direction
                     }
                 }
             }
-
+    
             // Calculate the bounding rectangle based on the data
             Vector2 bottomLeft = new Vector2(minX, minY);
             Vector2 topRight = new Vector2(maxX, maxY);
-
+    
             // Determine the size of the grid cells
             float cellSize = gridSize;
-
+    
             // Calculate the number of grid cells in x and z directions
             int xCellCount = Mathf.CeilToInt((topRight.x - bottomLeft.x) / cellSize);
             int zCellCount = Mathf.CeilToInt((topRight.y - bottomLeft.y) / cellSize);
-
-            // Create the grid vertices
-            vertices = new Vector3[(xCellCount + 1) * (zCellCount + 1)];
-
-            for (int z = 0, i = 0; z <= zCellCount; z++)
-            {
-                for (int x = 0; x <= xCellCount; x++)
-                {
-                    float xPos = bottomLeft.x + x * cellSize;
-                    float zPos = bottomLeft.y + z * cellSize;
-                    vertices[i] = new Vector3(xPos, 0, zPos);
-                    i++;
-                }
-            }
+    
+            // Calculate the size of the rectangle based on data and desired scale
+            float rectangleSizeX = (topRight.x - bottomLeft.x) / xCellCount;
+            float rectangleSizeZ = (topRight.y - bottomLeft.y) / zCellCount;
+    
+            // Set the desired visual scale (e.g., 1 meter in data corresponds to 1 unit in the mesh)
+            float desiredVisualScale = 1.0f;
+    
+            // Calculate the final rectangle size
+            float rectangleSize = Mathf.Max(rectangleSizeX, rectangleSizeZ) * desiredVisualScale;
+    
+            // Use this rectangle size for creating the mesh
+            CreateShape(rectangleSize);
         }
         catch (Exception e)
         {
@@ -81,52 +83,53 @@ public int zCellCount = 5;  // Number of quads in the z direction
         }
     }
 
-   void CreateShape()
-   {
-       // Calculate the size of each grid cell based on xCellCount and zCellCount
-       float cellSizeX = gridSize / xCellCount;
-       float cellSizeZ = gridSize / zCellCount;
-   
-       // Calculate the total number of vertices and triangles
-       int vertexCount = (xCellCount + 1) * (zCellCount + 1);
-       int triangleCount = xCellCount * zCellCount * 2 * 3;  // 2 triangles per quad, 3 vertices per triangle
-   
-       vertices = new Vector3[vertexCount];
-       triangles = new int[triangleCount];
-   
-       int vert = 0;
-       int tris = 0;
-   
-       for (int z = 0; z <= zCellCount; z++)
-       {
-           for (int x = 0; x <= xCellCount; x++)
-           {
-               float xPos = x * cellSizeX;
-               float zPos = z * cellSizeZ;
-               vertices[vert] = new Vector3(xPos, 0, zPos);
-   
-               // Define the triangles based on the current vertex and grid structure
-               if (x < xCellCount && z < zCellCount)
-               {
-                   int topLeft = vert;
-                   int topRight = vert + 1;
-                   int bottomLeft = vert + xCellCount + 1;
-                   int bottomRight = vert + xCellCount + 2;
-   
-                   triangles[tris + 0] = topLeft;
-                   triangles[tris + 1] = bottomLeft;
-                   triangles[tris + 2] = topRight;
-                   triangles[tris + 3] = topRight;
-                   triangles[tris + 4] = bottomLeft;
-                   triangles[tris + 5] = bottomRight;
-   
-                   tris += 6;
-               }
-   
-               vert++;
-           }
-       }
-   }
+void CreateShape(float rectangleSize)
+{
+    // Calculate the size of each grid cell based on xCellCount and zCellCount
+    float cellSizeX = rectangleSize / xCellCount;
+    float cellSizeZ = rectangleSize / zCellCount;
+
+    // Calculate the total number of vertices and triangles
+    int vertexCount = (xCellCount + 1) * (zCellCount + 1);
+    int triangleCount = xCellCount * zCellCount * 2 * 3;  // 2 triangles per quad, 3 vertices per triangle
+
+    vertices = new Vector3[vertexCount];
+    triangles = new int[triangleCount];
+
+    int vert = 0;
+    int tris = 0;
+
+    for (int z = 0; z <= zCellCount; z++)
+    {
+        for (int x = 0; x <= xCellCount; x++)
+        {
+            float xPos = x * cellSizeX;
+            float zPos = z * cellSizeZ;
+            vertices[vert] = new Vector3(xPos, 0, zPos);
+
+            // Define the triangles based on the current vertex and grid structure
+            if (x < xCellCount && z < zCellCount)
+            {
+                int topLeft = vert;
+                int topRight = vert + 1;
+                int bottomLeft = vert + xCellCount + 1;
+                int bottomRight = vert + xCellCount + 2;
+
+                triangles[tris + 0] = topLeft;
+                triangles[tris + 1] = bottomLeft;
+                triangles[tris + 2] = topRight;
+                triangles[tris + 3] = topRight;
+                triangles[tris + 4] = bottomLeft;
+                triangles[tris + 5] = bottomRight;
+
+                tris += 6;
+            }
+
+            vert++;
+        }
+    }
+}
+
 
     void UpdateMesh()
     {

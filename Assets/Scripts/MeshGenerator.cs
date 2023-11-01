0e7de9fe33b559bb454c6ffed9bca04961a6d65c
Thomasdalsae <72367829+Thomasdalsae@users.noreply.github.com>
2023-11-01 14:16:43 +0100

    Working version

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -2,6 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
+using System.IO;
+using System;
+using File = System.IO.File;
+
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
@@ -11,19 +16,67 @@ public class MeshGenerator : MonoBehaviour
     
     private Vector3[] vertices;
     private int[] triangles;
+    public TextAsset textAsset;
 
     public int xSize = 20;
     public int zSize = 20;
+
+    public float rectangleSize = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-
+       GetCoordFromFile(textAsset);
         CreateShape();
         UpdateMesh();
     }
 
+    void GetCoordFromFile(TextAsset path)
+    {   //This will always be one since i have normalized the values of the txt file.
+        try
+        {
+            string[] lines = textAsset.text.Split('\n');
+
+            float minX = float.MaxValue;
+            float minY = float.MaxValue;
+            float maxX = float.MinValue;
+            float maxY = float.MinValue;
+
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split(' ');
+                if (parts.Length >= 2)
+                {
+                    if (float.TryParse(parts[0], out float x) && float.TryParse(parts[1], out float y))
+                    {
+                        minX = Mathf.Min(minX, x);
+                        minY = Mathf.Min(minY, y);
+                        maxX = Mathf.Max(maxX, x);
+                        maxY = Mathf.Max(maxY, y);
+                    }
+                }
+            }
+
+            float width = maxX - minX;
+            float height = maxX - minY;
+            rectangleSize = Mathf.Min(width, height) / Mathf.Max(xSize, zSize);
+
+            Vector2 bottomLeft = new Vector2(minX, minY);
+            Vector2 bottomRight = new Vector2(maxX, minY);
+            Vector2 topLeft = new Vector2(minX, maxY);
+            Vector2 topRight = new Vector2(maxX, maxY);
+
+            Debug.Log("Bottom left: " + bottomLeft);
+            Debug.Log("Bottom right: " + bottomRight);
+            Debug.Log("Top left: " + topLeft);
+            Debug.Log("Top Right: " + topRight);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error reading file: " + e.Message);
+        }
+    }
 
     void CreateShape()
     {
@@ -33,7 +86,7 @@ public class MeshGenerator : MonoBehaviour
         {
             for (int x = 0; x <= xSize; x++)
             {
-                vertices[i] = new Vector3(x, 0, z);
+                vertices[i] = new Vector3(x * rectangleSize , 0, z * rectangleSize );
                 i++;
             }
         }    

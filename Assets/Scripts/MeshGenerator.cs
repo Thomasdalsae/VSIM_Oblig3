0cce34110cd14b5eaf3d4f99f5abe5cedfebf743
Thomasdalsae <Thomasdalsae@gmail.com>
2023-11-04 04:17:11 +0100

    nice

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -1,7 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+<<<<<<< Updated upstream
 using UnityEngine;
+=======
+using System.Linq;
+using UnityEngine;
+
+>>>>>>> Stashed changes
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
@@ -11,6 +17,18 @@ public class MeshGenerator : MonoBehaviour
     
     private Vector3[] vertices;
     private int[] triangles;
+<<<<<<< Updated upstream
+=======
+
+    public float cubeSize = 0.005f;
+    public GameObject cubePrefab;
+    public GameObject centerCubePrefab;
+    public int xGridCellCount = 5; // Number of grid cells in the x direction
+    public int zGridCellCount = 5; // Number of grid cells in the z direction
+    public TextAsset textAsset;
+    public int gridSize = 5; // Resolution in meters
+    public float visualScale = 1.0f;
+>>>>>>> Stashed changes
 
     public int xSize = 20;
     public int zSize = 20;
@@ -19,6 +37,7 @@ public class MeshGenerator : MonoBehaviour
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
+<<<<<<< Updated upstream
 
         CreateShape();
         UpdateMesh();
@@ -30,14 +49,52 @@ public class MeshGenerator : MonoBehaviour
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
         
         for (int i =0, z = 0; z <= zSize; z++)
+=======
+        if (textAsset != null)
+        {
+            GetCoordsFromFile(textAsset);
+            InstantiateCubes();
+        }
+        else
+        {
+            Debug.LogError("No TextAsset provided.");
+        }
+    }
+
+
+    void GetCoordsFromFile(TextAsset textAsset)
+    {
+        string[] lines = textAsset.text.Split('\n');
+        List<Vector3> coords = new List<Vector3>();
+
+        float minX = float.MaxValue;
+        float minZ = float.MaxValue;
+        float maxX = float.MinValue;
+        float maxZ = float.MinValue;
+
+        foreach (var line in lines)
+>>>>>>> Stashed changes
         {
             for (int x = 0; x <= xSize; x++)
             {
+<<<<<<< Updated upstream
                 vertices[i] = new Vector3(x, 0, z);
                 i++;
+=======
+                if (float.TryParse(parts[0], out float x) && float.TryParse(parts[1], out float y) &&
+                    float.TryParse(parts[2], out float z))
+                {
+                    minX = Mathf.Min(minX, x);
+                    minZ = Mathf.Min(minZ, z);
+                    maxX = Mathf.Max(maxX, x);
+                    maxZ = Mathf.Max(maxZ, z);
+                    coords.Add(new Vector3(x, y, z));
+                }
+>>>>>>> Stashed changes
             }
         }    
 
+<<<<<<< Updated upstream
         triangles = new int[xSize * zSize * 6];
         int vert = 0;
         int tris = 0;
@@ -58,10 +115,116 @@ public class MeshGenerator : MonoBehaviour
             }
 
             vert++;
+=======
+        xGridCellCount = Mathf.CeilToInt((maxX - minX) / gridSize);
+        zGridCellCount = Mathf.CeilToInt((maxZ - minZ) / gridSize);
+
+        CreateMesh(coords, minX, minZ);
+    }
+
+    void CreateMesh(List<Vector3> coords, float minX, float minZ)
+    {
+        float cellSizeX = (coords.Max(v => v.x) - minX) / xGridCellCount;
+        float cellSizeZ = (coords.Max(v => v.z) - minZ) / zGridCellCount;
+
+        int vertexCount = (xGridCellCount + 1) * (zGridCellCount + 1);
+        int triangleCount = xGridCellCount * zGridCellCount * 2 * 3;
+
+        vertices = new Vector3[vertexCount];
+        triangles = new int[triangleCount];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z <= zGridCellCount; z++)
+        {
+            for (int x = 0; x <= xGridCellCount; x++)
+            {
+                float xPos = x * cellSizeX + minX;
+                float zPos = z * cellSizeZ + minZ;
+                float centerX = xPos + (cellSizeX / 2);
+                float centerZ = zPos + (cellSizeZ / 2);
+                float totalHeight = 0f;
+                int pointCount = 0;
+
+                foreach (Vector3 point in coords)
+                {
+                    if (point.x >= xPos && point.x < (xPos + cellSizeX) &&
+                        point.z >= zPos && point.z < (zPos + cellSizeZ))
+                    {
+                        totalHeight += point.y;
+                        pointCount++;
+                    }
+                }
+
+                float averageHeight = (pointCount > 0) ? totalHeight / pointCount : 0f;
+                vertices[vert] = new Vector3(centerX, averageHeight, centerZ);
+
+                if (x < xGridCellCount && z < zGridCellCount)
+                {
+                    int topLeft = vert;
+                    int topRight = vert + 1;
+                    int bottomLeft = vert + xGridCellCount + 1;
+                    int bottomRight = vert + xGridCellCount + 2;
+
+                    triangles[tris + 0] = topLeft;
+                    triangles[tris + 1] = bottomLeft;
+                    triangles[tris + 2] = topRight;
+                    triangles[tris + 3] = topRight;
+                    triangles[tris + 4] = bottomLeft;
+                    triangles[tris + 5] = bottomRight;
+
+                    tris += 6;
+                }
+
+                vert++;
+            }
+        }
+
+        UpdateMesh();
+    }
+
+void InstantiateCubes()
+    {
+        if (cubePrefab != null && centerCubePrefab != null)
+        {
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                InstantiateCube(cubePrefab, vertices[i]);
+                if (i % (xGridCellCount + 1) < xGridCellCount && i < vertices.Length - xGridCellCount - 1)
+                {
+                    // Calculate the indices for the center of the square created by two triangles
+                    int centerIndex1 = i;
+                    int centerIndex2 = i + xGridCellCount + 2;
+                    Vector3 center = (vertices[centerIndex1] + vertices[centerIndex2]) * 0.5f;
+                    InstantiateRedCube(center);
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("Cube prefabs are not assigned.");
+>>>>>>> Stashed changes
         }
 
     }
 
+<<<<<<< Updated upstream
+=======
+    void InstantiateCube(GameObject prefab, Vector3 position)
+    {
+        GameObject cube = Instantiate(prefab, position, Quaternion.identity);
+        cube.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+    }
+
+    void InstantiateRedCube(Vector3 position)
+    {
+        GameObject cube = Instantiate(centerCubePrefab, position, Quaternion.identity);
+        cube.transform.localScale = new Vector3(cubeSize * 1.5f, cubeSize * 1.5f, cubeSize * 1.5f); // Adjust the scale
+        cube.GetComponent<Renderer>().material.color = Color.red; // Set the cube color to red
+    }
+
+>>>>>>> Stashed changes
     void UpdateMesh()
     {
         mesh.Clear();
@@ -73,3 +236,11 @@ public class MeshGenerator : MonoBehaviour
     }
 
 }
+
+
+
+
+
+
+
+
--- Assets/Scripts/PCGenerator.cs
+++ Assets/Scripts/PCGenerator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(TextAsset))]
 public class PCGenerator : MonoBehaviour
 {
@@ -9,23 +10,42 @@ public class PCGenerator : MonoBehaviour
 
     void Start()
     {
-        if (pointData != null) // checking if there is a TextAsset
+        if (pointData != null)
         {
-            string[] lines = pointData.text.Split('\n'); // Split on each new line;
-            
-            for (int i = 1; i < lines.Length; i++) 
+            string[] lines = pointData.text.Split('\n');
+
+            for (int i = 1; i < lines.Length; i++)
             {
                 string line = lines[i];
-                string[] values = line.Split(' '); // Split on each space
+                string[] values = line.Split(' ');
 
                 if (values.Length >= 3)
                 {
-                    //Getting x y z value from txt
                     float x = float.Parse(values[0]);
                     float y = float.Parse(values[1]);
                     float z = float.Parse(values[2]);
+<<<<<<< Updated upstream
                     Vector3 position = new Vector3(x * 5, y * 5, z * 5); // times 5 to make it easier to see
                     Instantiate(pointPrefab, position, Quaternion.identity); // Make a sphere for each location
+=======
+                    Vector3 position = new Vector3(x, z, y);
+
+                    // Ensure the pointPrefab has a Renderer component
+                    Renderer prefabRenderer = pointPrefab.GetComponent<Renderer>();
+
+                    if (prefabRenderer != null)
+                    {
+                        // Enable GPU instancing on the sharedMaterial of the prefab's renderer
+                        Material pointMaterial = prefabRenderer.sharedMaterial;
+                        pointMaterial.enableInstancing = true;
+
+                        Instantiate(pointPrefab, position, Quaternion.identity);
+                    }
+                    else
+                    {
+                        Debug.LogError("pointPrefab is missing a Renderer component.");
+                    }
+>>>>>>> Stashed changes
                 }
             }
         }
